96e665d14bd6cab3047b42bc1a0f87ab976ef802
Zenon Zeniou <zzeniou86@gmail.com>
2014-01-26 17:02:49 +0000

    implemented simple directive
    Signed-off-by: Zenon Zeniou <zzeniou86@gmail.com>

--- angular-notifyjs.js
+++ angular-notifyjs.js
@@ -1,7 +1,6 @@
 'use strict';
 angular.module('notifyjs', [])
   .provider('notifyjs', function notifyjsProvider() {
-
     // this.defaults = {
     //   clickToHide: true,
     //   autoHide: true,
@@ -19,14 +18,24 @@ angular.module('notifyjs', [])
     //   hideDuration: 200,
     //   gap: 5
     // }
-
     this.defaults = {};
-
     this.$get = function notifyjsService() {
-
       var notifyjs = $.notify;
       notifyjs.defaults(this.defaults);
-
       return notifyjs;
     };
-  });
+  })
+  .directive('notifyjs', function(notifyjs) {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        var notif_message = attrs.notifyjs ? attrs.notifyjs : 'default message'
+        var notif_event = attrs.notifyOn ? attrs.notifyOn : 'click';
+        var notif_class = attrs.notifyClass ? attrs.notifyClass : undefined;
+        var notif_pos = attrs.notifyPos ? attrs.notifyPos : undefined;
+        element.on(notif_event, function(e) {
+          element.notify(notif_message, {className: notif_class, position: notif_pos});
+        });
+      }
+    };
+  })
